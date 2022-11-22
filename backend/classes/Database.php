9c6d927a72e1886c229d0efc7d3dc537e4a82313
Johannes <johannes.knickering@hotmail.com>
2022-11-22 20:16:56 +0100

    .

--- backend/classes/Database.php
+++ backend/classes/Database.php
@@ -2,10 +2,11 @@
 class Database extends mysqli {
     private $server = "mysql70.unoeuro.com";
     private $username = "ahusted_dk";
-    private $password = "243rbtHnydmfa6xgwFpk ";
+    private $password = "243rbtHnydmfa6xgwFpk";
     private $database = "ahusted_dk_db";
+    private $port = 3306;
 
     function __construct(){
-        parent::__construct($this->server, $this->username, $this->password, $this->database);
+        parent::__construct($this->server, $this->username, $this->password, $this->database, $this->port);
     }
 }
