ed042057d4aef0a8e52d68bf79feff22ca9019cc
Johannes <johannes.knickering@hotmail.com>
2022-11-22 11:41:56 +0100

    db update

--- backend/classes/Database.php
+++ backend/classes/Database.php
@@ -1,9 +1,9 @@
 <?php declare(strict_types=1);
 class Database extends mysqli {
     private $server = "mysql70.unoeuro.com";
-    private $username = "knickering_dk";
-    private $password = "k59EBbxtdah2g4GcFDpH";
-    private $database = "knickering_dk_db";
+    private $username = "ahusted_dk";
+    private $password = "243rbtHnydmfa6xgwFpk ";
+    private $database = "ahusted_dk_db";
 
     function __construct(){
         parent::__construct($this->server, $this->username, $this->password, $this->database);
