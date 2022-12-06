f9cf9130111309f8a7ba8624320b144f7a1a348e
Johannes <johannes.knickering@hotmail.com>
2022-12-06 13:59:46 +0100

    .

--- frontend/src/pages/Restaurant/components/CreateBookingBtn/utils.ts
+++ frontend/src/pages/Restaurant/components/CreateBookingBtn/utils.ts
@@ -5,7 +5,9 @@
 
 export const getCurrentTime = () => {
     const date = new Date();
-    return date.toLocaleTimeString().slice(0, -6);
+    const hours = addZero(date.getHours());
+    const Minutes = addZero(date.getMinutes());
+    return hours + ':' + Minutes
 }
 
 /**
@@ -15,4 +17,11 @@ export const getCurrentTime = () => {
 export const getCurrentDate = () => {
     let date = new Date()
     return date.toISOString().split('T')[0]
+}
+
+function addZero(i: number | string) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
 }
